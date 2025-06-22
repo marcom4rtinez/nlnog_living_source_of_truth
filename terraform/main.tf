6b276c921010093c6d8dba4dadb95b59fda45ec1
Marco Martinez <mmmartinez@bluewin.ch>
2025-06-22 21:32:18 +0200

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "infrahub" {
-  api_key         = "184b52a5-a01a-fd77-3025-c51534fc8c7c"
+  api_key         = "184b73b6-77a8-2161-2dcf-c512a7d01746"
   infrahub_server = "http://localhost:8000"
   branch          = "main"
 }
@@ -30,7 +30,6 @@ data "infrahub_devicetype" "nokia_leafs" {
   device_type_name = "7220 IXR-D2L"
 }
 
-
 data "infrahub_autonomoussystem" "verizon" {
   as_name = "AS701"
 }
@@ -39,26 +38,10 @@ data "infrahub_platform" "srlinux" {
   platform_name = "Nokia SR Linux"
 }
 
-# resource "infrahub_ipaddress" "mgmt_address" {
-#   address_value = "10.0.0.100/32"
-#   description_value = "my cool ip"
-# }
-
-data "infrahub_ipaddressquery" "mgmt_address_0" {
+data "infrahub_ipaddressquery" "mgmt_address" {
   ip_address_value = "10.0.0.1/32"
 }
 
-data "infrahub_ipaddressquery" "mgmt_address_1" {
-  ip_address_value = "10.0.0.1/32"
-}
-
-locals {
-
-  mgmt_map = {
-    spine1 = data.infrahub_ipaddressquery.mgmt_address_0.id
-    spine2 = data.infrahub_ipaddressquery.mgmt_address_1.id
-  }
-}
 resource "infrahub_device" "create_spines" {
   for_each                = toset([for i in range(1, 3) : format("spine%d", i)])
   name_value              = each.key
@@ -66,47 +49,51 @@ resource "infrahub_device" "create_spines" {
   device_type_node_id     = data.infrahub_devicetype.nokia_spines.id
   location_node_id        = data.infrahub_country.usa.id
   platform_node_id        = data.infrahub_platform.srlinux.id
-  primary_address_node_id = local.mgmt_map[each.key]
+  primary_address_node_id = data.infrahub_ipaddressquery.mgmt_address.id
   status_value            = "active"
   topology_node_id        = data.infrahub_topology.fra05-pod1.id
   role_value              = "spine"
 }
 
-resource "infrahub_l3interface" "ethernet1-1" {
-  for_each          = toset([for i in range(1, 2) : format("spine%d", i)])
-  description_value = format("%s - ethernet-1/1", each.key)
-  role_value        = "leaf"
-  enabled_value     = true
-  name_value        = "ethernet-1/1"
-  device_node_id    = infrahub_device.create_spines[each.key].id
-  status_value      = "active"
+
+##############################################################################################################
+#                                         Create Spines                                                      #
+##############################################################################################################
+
+locals {
+  spines     = ["spine1", "spine2"]
+  interfaces = [1, 2, 3]
+
+  spine_interface_pairs = flatten([
+    for spine in local.spines : [
+      for intf in local.interfaces : {
+        spine     = spine
+        intf      = intf
+        intf_name = format("ethernet-1/%d", intf)
+        ip_addr   = format("192.168.%d%s.1/31", intf, substr(spine, -1, 1))
+      }
+    ]
+  ])
 }
 
-resource "infrahub_l3interface" "ethernet1-1-spine2" {
-  description_value = "spine2 - ethernet-1/1"
+resource "infrahub_l3interface" "ethernet" {
+  for_each = {
+    for pair in local.spine_interface_pairs :
+    "${pair.spine}-${pair.intf}" => pair
+  }
+
+  name_value        = each.value.intf_name
+  description_value = "${each.value.spine} - ${each.value.intf_name}"
   role_value        = "leaf"
   enabled_value     = true
-  name_value        = "ethernet-1/1"
-  device_node_id    = infrahub_device.create_spines["spine2"].id
+  device_node_id    = infrahub_device.create_spines[each.value.spine].id
   status_value      = "active"
-  full_ipv4_value   = "192.168.12.1/31"
+  full_ipv4_value   = each.value.ip_addr
 }
 
-
-
-
-resource "infrahub_l3interface" "ethernet1-2" {
-  for_each          = toset([for i in range(1, 3) : format("spine%d", i)])
-  description_value = format("%s - ethernet-1/2", each.key)
-  role_value        = "leaf"
-  enabled_value     = true
-  name_value        = "ethernet-1/2"
-  device_node_id    = infrahub_device.create_spines[each.key].id
-  status_value      = "active"
-}
-
-
-
+##############################################################################################################
+#                                         Create Leafs                                                       #
+##############################################################################################################
 
 resource "infrahub_device" "create_leaf" {
   for_each                = toset([for i in range(1, 4) : format("leaf%d", i)])
@@ -115,11 +102,12 @@ resource "infrahub_device" "create_leaf" {
   device_type_node_id     = data.infrahub_devicetype.nokia_leafs.id
   location_node_id        = data.infrahub_country.usa.id
   platform_node_id        = data.infrahub_platform.srlinux.id
-  primary_address_node_id = data.infrahub_ipaddressquery.mgmt_address_0.id
+  primary_address_node_id = data.infrahub_ipaddressquery.mgmt_address.id
   status_value            = "active"
   topology_node_id        = data.infrahub_topology.fra05-pod1.id
   role_value              = "leaf"
 }
+
 
 
 resource "infrahub_l2interface" "ethernet1-1" {
@@ -127,8 +115,40 @@ resource "infrahub_l2interface" "ethernet1-1" {
   description_value = format("%s - ethernet-1/1", each.key)
   l2_mode_value     = "Access"
   role_value        = "leaf"
-  enabled_value     = false
+  enabled_value     = true
   name_value        = "ethernet-1/1"
   device_node_id    = infrahub_device.create_leaf[each.key].id
   status_value      = "active"
 }
+
+
+# locals {
+#   leafs           = ["leaf1", "leaf2", "leaf3"]
+#   leaf_interfaces = [49, 50, 51, 52]
+
+#   leaf_interface_pairs = flatten([
+#     for leaf in local.leafs : [
+#       for intf in local.leaf_interfaces : {
+#         leaf      = leaf
+#         intf      = intf
+#         intf_name = format("ethernet-1/%d", intf)
+#         ip_addr   = format("192.168.%s%d.0/31", substr(leaf, -1, 1), intf - 48)
+#       }
+#     ]
+#   ])
+# }
+
+# resource "infrahub_l3interface" "leaf_uplinks" {
+#   for_each = {
+#     for pair in local.leaf_interface_pairs :
+#     "${pair.leaf}-${pair.intf}" => pair
+#   }
+
+#   name_value        = each.value.intf_name
+#   description_value = "${each.value.leaf} - ${each.value.intf_name}"
+#   role_value        = "leaf"
+#   enabled_value     = true
+#   device_node_id    = infrahub_device.create_leaf[each.value.leaf].id
+#   status_value      = "active"
+#   full_ipv4_value   = each.value.ip_addr
+# }
